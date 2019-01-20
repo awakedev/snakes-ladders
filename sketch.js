1de9736643ca650a296f1583d59940073de54109
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-20 13:05:52 +0000

    player and tile creation

--- sketch.js
+++ sketch.js
@@ -1,12 +1,45 @@
-
+let tiles = [];
+let player;
 
 function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(255, 0, 200);
-      }
+    createCanvas(400, 400);
+
+    let resolution = 100;
+    let cols = width / resolution;
+    let rows = height / resolution;
+
+    let x = 0;
+    let y = (rows - 1) * resolution;
+    let dir = 1;
+    for (let i = 0; i < cols * rows; i++) {
+        let tile = new Tile(x, y, resolution, i, i + 1);
+        tiles.push(tile);
+        x = x + (resolution * dir);
+        if (x >= width || x <= -resolution) {
+            dir *= -1;
+            x += resolution * dir;
+            y -= resolution;
+        }
+    }
+
+    player = new Player()
+}
 
 
 function draw() {
-    background(0);
+    background(51);
 
-        }
+    for (let tile of tiles) {
+        tile.show();
+
+    }
+    player.roll();
+
+    if (player.spot >= tiles.length-1) {
+        player.spot = tiles.length-1;
+        noLoop()
+    }
+
+    player.show(tiles);
+
+}
---
+++ tile.js
@@ -0,0 +1,29 @@
+class Tile {
+    constructor(x, y, wh, index, next)
+    {
+        this.x = x;
+        this.y = y
+        this.wh = wh;
+        this.next = next;
+        this.index = index;
+        this.color = random(255);
+        if (this.index % 2 == 0){
+            this.color = 220;
+
+        } else {
+            this.color = 30;
+        }
+    }
+
+    getCenter(){
+        let cx = this.x + this.wh / 2;
+        let cy = this.y / this.wh / 2;
+        return [cx, cy];
+    }
+
+    show(){
+        fill(this.color);
+        rect(this.x, this.y, this.wh, this.wh);
+    }
+    
+}
